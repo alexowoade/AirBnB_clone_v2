60160ca77cb9ff0ebef22aba6949a9e29b676dff
Jesse <ibbraidejesse@gmail.com>
2022-10-27 15:31:39 +0100

    testing fabric

---
+++ 1-pack_web_static.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# generates a .tgz archive from the contents of the web_static folder
+from fabric.api import local
+import time
+
+def do_pack():
+    """  """
+    local('mkdir -p versions')
+    arch_name = f'web_static_{time.strftime("%Y%m%d%H%M%S")}.tgz'
+    if local(f'tar -cvzf {arch_name} web_static'):
+        return arch_name
+    return None
