beee3fb3a48347f9b311e33d881e374e111b8a41
Jesse <ibbraidejesse@gmail.com>
2022-10-29 21:03:49 +0100

    generates a .tgz archive

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -4,9 +4,9 @@ from fabric.api import local
 import time
 
 def do_pack():
-    """  """
+    """ generates a .tgz archive """
     local('mkdir -p versions')
-    arch_name = f'web_static_{time.strftime("%Y%m%d%H%M%S")}.tgz'
+    arch_name = f'versions/web_static_{time.strftime("%Y%m%d%H%M%S")}.tgz'
     if local(f'tar -cvzf {arch_name} web_static'):
         return arch_name
     return None
