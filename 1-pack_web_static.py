2ab9c5263e791bd2cb8c70cddeeaaf33b2542851
Jesse <ibbraidejesse@gmail.com>
2022-10-29 21:29:23 +0100

    edited documentation

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""generates a .tgz archive from the contents of the web_static folder"""
+""" generates a .tgz archive from the contents of the web_static folder """
 from fabric.api import local
 import time
 
