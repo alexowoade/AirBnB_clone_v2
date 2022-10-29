8bb9c526be59e0e8ae1cf05b1ab6738554b2513f
Jesse <ibbraidejesse@gmail.com>
2022-10-29 21:35:33 +0100

    changed to datetime

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-""" generates a .tgz archive from the contents of the web_static folder """
+"""generates a .tgz archive from the contents of the web_static folder"""
 from fabric.api import local
-import time
+from datetime import datetime
 
 
 def do_pack():
     """ returns generated archive else None """
-    arch_path = f'versions/web_static_{time.strftime("%Y%m%d%H%M%S")}.tgz'
+    local('mkdir -p versions')
+    arch_path = f'versions/web_static_{datetime.now().strftime("%Y%m%d%H%M%S")}.tgz'
     if local(f'tar -cvzf {arch_path} web_static'):
         return arch_path
     return None
