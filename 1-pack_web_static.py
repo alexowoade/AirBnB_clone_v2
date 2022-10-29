fc09e2d12744e9679b8e4e0b824d498498432935
Jesse <ibbraidejesse@gmail.com>
2022-10-29 21:49:47 +0100

    troubleshooting

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """generates a .tgz archive from the contents of the web_static folder"""
 from fabric.api import local
-from datetime import datetime
+import time
 
 
 def do_pack():
     """ returns generated archive else None """
     local('mkdir -p versions')
-    arch_path = f'versions/web_static_{datetime.now().strftime("%Y%m%d%H%M%S")}.tgz'
+    arch_path = f'versions/web_static_{time.strftime("%Y%m%d%H%M%S")}.tgz'
     if local(f'tar -cvzf {arch_path} web_static'):
         return arch_path
     return None
