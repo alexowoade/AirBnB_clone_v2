3a4193e0d38a55201ea7c98a84c36f2424be42fb
Jesse <ibbraidejesse@gmail.com>
2022-10-30 20:05:22 +0100

    refactored code

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -2,9 +2,10 @@
 """ uses the do_pack and do_deploy functions for full deployment """
 import time
 from os import path
-from fabric.api import local, put, run, cd, env
+from fabric.api import *
 
 env.hosts = ['34.204.186.164', '54.80.223.108']
+env.user = 'ubuntu'
 
 
 def do_pack():
@@ -25,24 +26,34 @@ def do_deploy(archive_path):
     """
     if not path.exists(archive_path):
         return False
+
     archive_name = archive_path[9:]
+
     remote_dir = '/data/web_static/releases/' + archive_name[:-4]
+
     put(archive_path, '/tmp')
+
     run('sudo mkdir -p {}'.format(remote_dir))
+
     with cd(remote_dir):
         run('sudo tar -xzf {}'.format('/tmp/' + archive_name))
+
     run('sudo rm /tmp/{}'.format(archive_name))
+
     run('sudo mv {}/web_static/* {}'.format(remote_dir, remote_dir))
+
     run('sudo rm -rf {}/web_static'.format(remote_dir))
+
     run('sudo rm /data/web_static/current')
+
     run('sudo ln -s {} /data/web_static/current'.format(remote_dir))
+
     print("New version deployed!")
+
     return True
 
 
 def deploy():
     """ full deployment """
     archive_path = do_pack()
-    if archive_path:
-        do_deploy(archive_path)
-    return False
+    return do_deploy(archive_path) if archive_path else False
