cb79a707b5abf9b6059461a9eb1058b16a1066c8
Jesse <ibbraidejesse@gmail.com>
2022-10-30 07:10:31 +0100

    troubleshooting

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -10,12 +10,11 @@ def do_pack():
         gernerated.
     """
 
-    local("mkdir -p versions")
+    local('mkdir -p versions')
     date = datetime.now().strftime("%Y%m%d%H%M%S")
     archived_f_path = "versions/web_static_{}.tgz".format(date)
     t_gzip_archive = local("tar -cvzf {} web_static".format(archived_f_path))
 
     if t_gzip_archive.succeeded:
         return archived_f_path
-    else:
-        return None
+    return None
