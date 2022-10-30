bb86852876619809b365390fedbe56b060c9969d
Jesse <ibbraidejesse@gmail.com>
2022-10-30 20:18:10 +0100

    refactored code

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """ uses the do_pack and do_deploy functions for full deployment """
+from fabric.api import *
 import time
 from os import path
-from fabric.api import *
+
 
 env.hosts = ['34.204.186.164', '54.80.223.108']
 env.user = 'ubuntu'
@@ -10,11 +11,9 @@ env.user = 'ubuntu'
 
 def do_pack():
     """ returns .tgz archive if generated else None """
-
     local('mkdir -p versions')
     time_created = time.strftime("%Y%m%d%H%M%S")
     arch_path = 'versions/web_static_{}.tgz'.format(time_created)
-
     if local('tar -cvzf {} web_static'.format(arch_path)):
         return arch_path
     return None
@@ -26,30 +25,18 @@ def do_deploy(archive_path):
     """
     if not path.exists(archive_path):
         return False
-
     archive_name = archive_path[9:]
-
     remote_dir = '/data/web_static/releases/' + archive_name[:-4]
-
     put(archive_path, '/tmp')
-
     run('sudo mkdir -p {}'.format(remote_dir))
-
     with cd(remote_dir):
         run('sudo tar -xzf {}'.format('/tmp/' + archive_name))
-
     run('sudo rm /tmp/{}'.format(archive_name))
-
     run('sudo mv {}/web_static/* {}'.format(remote_dir, remote_dir))
-
     run('sudo rm -rf {}/web_static'.format(remote_dir))
-
     run('sudo rm /data/web_static/current')
-
     run('sudo ln -s {} /data/web_static/current'.format(remote_dir))
-
     print("New version deployed!")
-
     return True
 
 
