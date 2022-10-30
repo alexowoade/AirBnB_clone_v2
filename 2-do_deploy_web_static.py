88905fc8d46006be97bca5d55fad0c7d059d2d13
Jesse <ibbraidejesse@gmail.com>
2022-10-30 16:16:47 +0100

    testing

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """ distributes an archive to web servers """
 from os import path
-from fabric.api import put, run, cd
+from fabric.api import put, run, cd, env
 
-env.hosts = ['34.204.186.164', '54.80.223.108']
+env.hosts = ['34.204.186.164']
 env.user = 'ubuntu'
 
-
+# web_static_20221030133540.tgz
 def do_deploy(archive_path):
     """ returns True if all operations are successful
         or False if file path doesn't exist
