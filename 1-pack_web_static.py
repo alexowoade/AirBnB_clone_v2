33bfb48fa24863081424e5488edc132c2cbfdd4d
Jesse <ibbraidejesse@gmail.com>
2022-10-30 07:17:32 +0100

    changed to time

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -11,8 +11,10 @@ def do_pack():
     """
 
     local('mkdir -p versions')
-    archived_f_path = f'versions/web_static_{time.strftime("%Y%m%d%H%M%S")}.tgz'
+    date = time.strftime("%Y%m%d%H%M%S")
+    archived_f_path = "versions/web_static_{}.tgz".format(date)
     t_gzip_archive = local("tar -cvzf {} web_static".format(archived_f_path))
+
     if t_gzip_archive:
         return archived_f_path
     return None
