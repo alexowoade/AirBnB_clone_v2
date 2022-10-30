4d47af7f0a9ac947ff616c52dbdc29521a1046a8
Jesse <ibbraidejesse@gmail.com>
2022-10-30 13:40:05 +0100

    formatted code

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -6,9 +6,11 @@ import time
 
 def do_pack():
     """ returns .tgz archive if generated else None """
+
     local('mkdir -p versions')
     time_created = time.strftime("%Y%m%d%H%M%S")
     arch_path = 'versions/web_static_{}.tgz'.format(time_created)
+
     if local('tar -cvzf {} web_static'.format(arch_path)):
         return arch_path
     return None
