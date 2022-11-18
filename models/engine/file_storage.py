1f170f3db54941a343bbe455adb035713a97100f
Jesse <ibbraidejesse@gmail.com>
2022-11-18 20:27:37 +0100

    debugging3

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -19,7 +19,7 @@ class FileStorage:
         if cls is None:
             return FileStorage.__objects
         cls_dict = {}
-        objs_dict = FileStorage.__objects
+        objs_dict = self.__objects
         for key, obj in objs_dict:
             if obj.__class__.__name__ == cls.__name__:
                 cls_dict[key] = obj
