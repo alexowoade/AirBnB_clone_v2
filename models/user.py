692f4cab3fedb71566432fe15c11f06fdaefb3d6
Jesse <ibbraidejesse@gmail.com>
2022-11-19 20:36:25 +0100

    modified its class attributes

--- models/user.py
+++ models/user.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
@@ -10,3 +11,6 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
+
+    places = relationship('Place', backref='user',
+                          cascade='all, delete-orphan')
