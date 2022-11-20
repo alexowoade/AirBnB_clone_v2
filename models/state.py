18a318615fb9992b09a50cd67a070bf0281a1b04
Jesse <ibbraidejesse@gmail.com>
2022-11-20 17:56:33 +0100

    added delete to cascade

--- models/state.py
+++ models/state.py
@@ -14,7 +14,7 @@ class State(BaseModel, Base):
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
     cities = relationship('City', backref='state',
-                          cascade='all, delete-orphan')
+                          cascade='all, delete, delete-orphan')
 
     # file storage
     @property
