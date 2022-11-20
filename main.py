343e986abf62d0b818bf05bcff8781a82dd4d7a8
Jesse <ibbraidejesse@gmail.com>
2022-11-20 17:51:02 +0100

    added storage swap

--- main.py
+++ main.py
@@ -1,37 +1,82 @@
 #!/usr/bin/python3
-"""
- Test cities access from a state
-"""
-from models import storage
-from models.state import State
-from models.city import City
+import inspect
+import io
+import sys
+import cmd
+import shutil
+import os
 
 """
- Objects creations
+ Backup console file
 """
-state_1 = State(name="California")
-print("New state: {}".format(state_1))
-state_1.save()
-state_2 = State(name="Arizona")
-print("New state: {}".format(state_2))
-state_2.save()
-
-city_1_1 = City(state_id=state_1.id, name="Napa")
-print("New city: {} in the state: {}".format(city_1_1, state_1))
-city_1_1.save()
-city_1_2 = City(state_id=state_1.id, name="Sonoma")
-print("New city: {} in the state: {}".format(city_1_2, state_1))
-city_1_2.save()
-city_2_1 = City(state_id=state_2.id, name="Page")
-print("New city: {} in the state: {}".format(city_2_1, state_2))
-city_2_1.save()
+if os.path.exists("tmp_console_main.py"):
+    shutil.copy("tmp_console_main.py", "console.py")
+shutil.copy("console.py", "tmp_console_main.py")
 
 
 """
- Verification
+ Updating console to remove "__main__"
 """
-print("")
-all_states = storage.all(State)
-for state_id, state in all_states.items():
-    for city in state.cities:
-        print("Find the city {} in the state {}".format(city, state))
+with open("tmp_console_main.py", "r") as file_i:
+    console_lines = file_i.readlines()
+    with open("console.py", "w") as file_o:
+        in_main = False
+        for line in console_lines:
+            if "__main__" in line:
+                in_main = True
+            elif in_main:
+                if "cmdloop" not in line:
+                    file_o.write(line.lstrip("    "))
+            else:
+                file_o.write(line)
+
+import console
+
+
+"""
+ Create console
+"""
+console_obj = "HBNBCommand"
+for name, obj in inspect.getmembers(console):
+    if inspect.isclass(obj) and issubclass(obj, cmd.Cmd):
+        console_obj = obj
+
+my_console = console_obj(stdout=io.StringIO(), stdin=io.StringIO())
+my_console.use_rawinput = False
+
+
+"""
+ Exec command
+"""
+def exec_command(my_console, the_command, last_lines = 1):
+    my_console.stdout = io.StringIO()
+    real_stdout = sys.stdout
+    sys.stdout = my_console.stdout
+    my_console.onecmd(the_command)
+    sys.stdout = real_stdout
+    lines = my_console.stdout.getvalue().split("\n")
+    return "\n".join(lines[(-1*(last_lines+1)):-1])
+
+
+"""
+ Tests
+"""
+state_id = exec_command(my_console, "create State name=\"California\"")
+if state_id is None or state_id == "":
+    print("FAIL: Can't create State")
+
+city_id = exec_command(my_console, "create City state_id=\"{}\" name=\"Fremont\"".format(state_id))
+if city_id is None or city_id == "":
+    print("FAIL: Can't create City")
+
+user_id = exec_command(my_console, "create User email=\"a@a.com\" password=\"pwd\" first_name=\"fn\" last_name=\"ln\"")
+if user_id is None or user_id == "":
+    print("FAIL: Can't create User")
+
+place_id = exec_command(my_console, "create Place city_id=\"{}\" user_id=\"{}\" name=\"House\"".format(city_id, user_id))
+if place_id is None or place_id == "":
+    print("FAIL: Can't create Place")
+
+print("OK", end="")
+
+shutil.copy("tmp_console_main.py", "console.py")
--- models/city.py
+++ models/city.py
@@ -2,13 +2,19 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
+from models import storage_type
 
 
 class City(BaseModel, Base):
     ''' defines City class '''
-    __tablename__ = 'cities'
-    name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    if storage_type == 'db':
+        __tablename__ = 'cities'
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
 
-    places = relationship('Place', backref='cities',
-                          cascade='all, delete, delete-orphan')
+        places = relationship('Place', backref='cities',
+                              cascade='all, delete, delete-orphan')
+
+    else:
+        name = ''
+        state_id = ''
