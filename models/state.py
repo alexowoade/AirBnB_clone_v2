484064bd422811335131e1ea8292c6788bc5d3ac
Jesse <ibbraidejesse@gmail.com>
2022-11-20 18:14:20 +0100

    added storage swap

--- models/state.py
+++ models/state.py
@@ -4,21 +4,22 @@ from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 import models
 from models.city import City
+from models import storage_type
 
 
 class State(BaseModel, Base):
     ''' defines State class '''
-    # name = ''
-
     # db storage
     __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
-    cities = relationship('City', backref='state',
+    if storage_type == 'db':
+        name = Column(String(128), nullable=False)
+        cities = relationship('City', backref='state',
                           cascade='all, delete, delete-orphan')
-
     # file storage
-    @property
-    def cities(self):
-        all_cities = models.storage.all(City)
-        return [city for city in all_cities.values()
-                if city.state_id == self.id]
+    else:
+        name = ''
+        @property
+        def cities(self):
+            all_cities = models.storage.all(City)
+            return [city for city in all_cities.values()
+                    if city.state_id == self.id]
