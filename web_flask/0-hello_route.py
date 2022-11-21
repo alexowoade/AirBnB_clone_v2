f1c81b039463ebbacd39375e6a2458f2b4a39482
Jesse <ibbraidejesse@gmail.com>
2022-11-21 11:55:26 +0100

    first flask web app

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,8 +1,3 @@
-#!/usr/bin/python3
-"""
-A script that starts a Flask web application:
-"""
-
 from flask import Flask
 
 app = Flask(__name__)
@@ -10,13 +5,9 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_route():
-    """
-    Displays 'Hello HBNB!'
-    Returns:
-        str: "Hello HBNB"
-    """
-    return "Hello HBNB!"
+    ''' starts my first flask web application '''
+    return 'Hello HBNB!'
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
